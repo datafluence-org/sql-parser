6324f8e770e92173b71670964256b2244ddf1d45
Wrisov Karmakar <wrisovkarmakar@Wrisovs-MacBook-Air.local>
2023-04-26 23:01:52 +0530

    Initial setup for Spring

---
+++ src/main/java/com/dataconfluence/sqlparser/SqlParserApplication.java
@@ -0,0 +1,14 @@
+package com.dataconfluence.sqlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class SqlParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataconfluence/sqlparser/SqlParserApplicationTests.java
@@ -0,0 +1,10 @@
+package com.dataconfluence.sqlparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlParserApplicationTests {
+
+
+}
